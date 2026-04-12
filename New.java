fb23e412a94111990430e8cf849697cd2ecaf168
yk663343-cell <yk663343@gmail.com>
2026-04-12 10:11:10 +0530

    Pattern

---
+++ New.java
@@ -0,0 +1,12 @@
+public class New{
+    public static void main(String arg []){
+        int a=1;
+        int b=2;
+        int sum = a+b;
+        System.out.println(sum);
+        for(int i=1;i<=5;i++){
+            System.out.println(i+sum);
+        }
+
+    }
+}
