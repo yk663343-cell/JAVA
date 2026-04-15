d3d798e91941a19e8ef07465c8b8f3adaa6ac669
yk663343-cell <yk663343@gmail.com>
2026-04-16 00:44:16 +0530

    Frequency

---
+++ NumberPattern.java
@@ -0,0 +1,11 @@
+public class NumberPattern{
+    public static void main(String arg[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }
+}
