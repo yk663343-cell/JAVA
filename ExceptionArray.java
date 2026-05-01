6e942147c0f0687e222b0e340f463ca8cb97b094
yk663343-cell <yk663343@gmail.com>
2026-05-01 06:44:32 +0530

    Added Java exception handling code

---
+++ ExceptionArray.java
@@ -0,0 +1,10 @@
+public class ExceptionArray {
+    public static void main(String[] args) {
+        try {
+            int arr[] = {1, 2, 3};
+            System.out.println(arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("array index is not valid.");
+        }
+    }
+}
