6c7900f64abb0eb35ab8c8a4143faadefff4196e
yk663343-cell <yk663343@gmail.com>
2026-05-06 23:56:46 +0530

    vowel and consonant ditector

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class VowelConsonant {
     public static void main(String[] args) {
@@ -8,8 +8,9 @@ public class VowelConsonant {
         System.out.print("Enter a character: ");
         char ch = sc.next().charAt(0);
 
-        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
-           ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u' ||
+            ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+
             System.out.println("Vowel");
         } else {
             System.out.println("Consonant");
