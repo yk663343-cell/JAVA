09e642bf3fc7511e9858520f0d405d1cd8838b98
yk663343-cell <yk663343@gmail.com>
2026-05-11 17:54:06 +0530

    frequency of char

---
+++ CharFrequency.java
@@ -0,0 +1,14 @@
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        char ch = 'g';
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+        }
+        System.out.println("The character '" + ch + "' appears " + count + " times.");
+    }
+}
---
+++ java.java
@@ -0,0 +1,9 @@
+public class java{
+    public static void main (String []arg ){
+        float a = 12345678910;
+        float b = 11121314151617181920;
+        float c = a*21222324252627282930;
+        System.out.println(c);
+
+    }
+}
