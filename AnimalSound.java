4d17daee05e670bc6874969176b415bd1fd78a33
yk663343-cell <yk663343@gmail.com>
2026-05-14 13:36:44 +0530

    overriding

---
+++ AnimalSound.java
@@ -0,0 +1,29 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class AnimalSound {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        
+        myAnimal.sound();
+        myDog.sound();
+        myCat.sound();
+    }
+}
