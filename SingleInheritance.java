9169622d77139c7832477a6066c3ab2d6897350b
yk663343-cell <yk663343@gmail.com>
2026-05-16 17:28:56 +0530

    single inheritance

---
+++ SingleInheritance.java
@@ -0,0 +1,19 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
